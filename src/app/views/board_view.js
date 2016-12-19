3dfc719657911077850c260bc9f7bcb315ff73db
Sarah Nahmias <nahmisa@gmail.com>
2016-12-19 11:30:46 -0800

    Adds a new X when clicked

--- src/app/views/board_view.js
+++ src/app/views/board_view.js
@@ -5,15 +5,11 @@ import SquareView from 'app/views/square_view';
 const BoardView = Backbone.View.extend({
   initialize: function(){
 
-    this.grid = this.model.get('grid');
-
     // we re-render the board when the model is updated (a square has been filled)
-    this.listenTo(this.model, 'update', this.render);
+    // this.listenTo(this.model, 'update', this.render);
 
     // change the player when we update the board by triggering a 'turn' event
-    this.listenTo(this.model, 'update', this.turn);
-
-
+    // this.listenTo(this.model, 'update', this.turn);
   },
 
   turn: function() {
@@ -23,15 +19,34 @@ const BoardView = Backbone.View.extend({
   addMarker: function(marker) {
   // triggers an event
     console.log('trying to add a marker ' + marker.model + ' at ' + marker.position);
+    this.row = marker.position[0];
+    this.column = marker.position[1];
+
+    this.grid = this.model.get('grid');
+
+    var boardPosition = this.grid[this.row][this.column];
+
+    if(boardPosition === null) {
+      this.grid[this.row][this.column] = 'X';
+
+      this.model.set('grid', this.grid);
+    }
+
+    console.log(this.model);
+
+  this.render();
   },
 
 
   render: function() {
 
     const boardSquares = Backbone.$('#board-squares');
+    boardSquares.empty();
     // loop within a loop - we need to have access to the larger this
 
+    console.log('render' + this.model);
     const self = this;
+    this.grid = this.model.get('grid');
 
     this.grid.forEach(function(row, index) {
 
@@ -48,6 +63,7 @@ const BoardView = Backbone.View.extend({
       }
 
     });
+
     return this;
   }
 
