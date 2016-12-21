7afddebaf26b34ea6c25e4c86ee4646737c58a20
Sarah Nahmias <nahmisa@gmail.com>
2016-12-21 11:01:00 -0800

    Removes some more comments

--- src/app/views/board_view.js
+++ src/app/views/board_view.js
@@ -4,12 +4,8 @@ import SquareView from 'app/views/square_view';
 
 const BoardView = Backbone.View.extend({
   initialize: function(options){
-    // this.model = options.model;
     // we re-render the board when the model is updated (a square has been filled)
     this.listenTo(this.model, 'change', this.render);
-
-    // change the player when we update the board by triggering a 'turn' event
-    // this.listenTo(this.model, 'update', this.turn);
   },
 
   turn: function() {
